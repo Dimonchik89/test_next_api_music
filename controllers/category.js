9fd038d75eff0fabd4c3b6dc460505350861253b
Dimonchik89 <dima.music1989@gmail.com>
2023-02-17 09:15:44 +0200

    add try catch to delete category and change

--- controllers/category.js
+++ controllers/category.js
@@ -40,20 +40,25 @@ const createCategory = async (req, res) => {
 }
 
 const deleteCategory = async (req, res) => {
-    const {id} = req.params
-    if(!id) {
-        return res.json({message: "Id is not defined"})
-    }
-    const category = await sequelize.models.category.findOne({where: { id }})
+    try {
+        const {id} = req.params
+        if(!id) {
+            return res.json({message: "Id is not defined"})
+        }
+        const category = await sequelize.models.category.findOne({where: { id }})
 
-    if(!category) {
-        return res.json({message: "Cetagory is not defined"})
-    }
-    fs.unlink(path.resolve(__dirname, "..", "static", category.img), async err => {
-        if(err) throw err;
-        const deletedCategory = await sequelize.models.category.destroy({where: { id }})
-        return res.json({message: `CetagoryId ${id} delete`})
+        if(!category) {
+            return res.json({message: "Cetagory is not defined"})
+        }
+        fs.unlink(path.resolve(__dirname, "..", "static", category.img), async err => {
+            if(err) throw err;
+            const deletedCategory = await sequelize.models.category.destroy({where: { id }})
+            return res.json({message: `CetagoryId ${id} delete`})
     })
+    } catch(e) {
+        throw new Error(e)
+    }
+
 }
 
 const changeCategory = async (req, res) => {
@@ -73,22 +78,25 @@ const changeCategory = async (req, res) => {
         return res.json({message: "Category is not defined"})
     }
 
-    if(img) {
-        fs.unlink(path.resolve(__dirname, "..", "static", category.img), async err => {
-            if(err) throw err
-            const fileExtension = img.name.split(".").pop()
-            let fileName = uuid.v4() + `.${fileExtension}`
-            img.mv(path.resolve(__dirname, "..", "static/category", fileName))
-            const updateCategory = await sequelize.models.category.update({...tailData, img: `category/${fileName}`}, { where: { id }})
+    try {
+        if(img) {
+            fs.unlink(path.resolve(__dirname, "..", "static", category.img), async err => {
+                if(err) throw err
+                const fileExtension = img.name.split(".").pop()
+                let fileName = uuid.v4() + `.${fileExtension}`
+                img.mv(path.resolve(__dirname, "..", "static/category", fileName))
+                const updateCategory = await sequelize.models.category.update({...tailData, img: `category/${fileName}`}, { where: { id }})
+                const newCategory = await sequelize.models.category.findOne({where: { id }})
+                return res.json(newCategory)
+            })
+        } else {
+            const updateCategory = await sequelize.models.category.update({...tailData}, { where: { id }})
             const newCategory = await sequelize.models.category.findOne({where: { id }})
             return res.json(newCategory)
-        })
-    } else {
-        const updateCategory = await sequelize.models.category.update({...tailData}, { where: { id }})
-        const newCategory = await sequelize.models.category.findOne({where: { id }})
-        return res.json(newCategory)
+        }
+    } catch(e) {
+        throw new Error(e)
     }
-
 }
 
 module.exports = {
