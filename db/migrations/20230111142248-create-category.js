9c60ecc340b6899611a7c0b483c26a559c39f667
Dimonchik89 <dima.music1989@gmail.com>
2023-02-17 11:17:17 +0200

    change id type

--- db/migrations/20230111142248-create-category.js
+++ db/migrations/20230111142248-create-category.js
@@ -7,7 +7,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.UUID
+        type: Sequelize.INTEGER
       },
       name: {
         type: Sequelize.STRING
