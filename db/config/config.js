8104eb04bc2135463cb92b1dd843d9ccd4ce8741
Dimonchik89 <dima.music1989@gmail.com>
2023-03-09 08:49:53 +0200

    change config file? remove variables and add static db configuration

--- db/config/config.js
+++ db/config/config.js
@@ -1,11 +1,25 @@
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+  // "development": {
+  //   "username": process.env.DB_USERNAME,
+  //   "password": process.env.DB_PASSWORD,
+  //   "database": process.env.DB_NAME,
+  //   "host": process.env.DB_HOST,
+  //   "dialect": "postgres",
+  //   "port": process.env.DB_PORT,
+  //    "dialectOptions": {
+  //     "ssl": { 
+  //       "require": true,
+  //       "rejectUnauthorized": false
+  //     }
+  //   }
+  // },
+    "development": {
+    "username": 'doadmin',
+    "password": 'AVNS_d2C-jKCYNawehoDwn5C',
+    "database": 'defaultdb',
+    "host": 'db-postgresql-fra1-22717-do-user-13608356-0.b.db.ondigitalocean.com',
     "dialect": "postgres",
-    "port": process.env.DB_PORT,
+    "port": 25060,
      "dialectOptions": {
       "ssl": { 
         "require": true,
