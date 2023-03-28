ba83ffa388bc58eac2da49461e8d2ae5faf75e04
Dimonchik89 <dima.music1989@gmail.com>
2023-03-28 13:54:11 +0300

    remove try catch in audio getAll

--- controllers/audio.js
+++ controllers/audio.js
@@ -43,7 +43,34 @@ const create = async (req, res) => {
 }
 
 const getAll = async (req, res) => {
-    try {
+    // try {
+    //     let {categoryId, keywords, limit, page} = req.query
+    //     // const lookupValue = keywords?.toLowerCase()
+
+    //     page = page || 1
+    //     limit = limit || 9
+    //     const offset = page * limit - limit
+    //     let audio;
+
+    //     if(categoryId && !keywords) {
+    //         audio = await sequelize.models.Audio.findAndCountAll({where: { categoryId: {[Op.like]: `%${categoryId}%`}}, limit, offset, order: [ [ 'createdAt', 'DESC' ]]})
+    //     } if(!categoryId && keywords) {
+    //         const keywordsArr = keywords.trim().split(" ")
+    //             ?.map(item => item.trim().replace(",", "").toLowerCase())
+    //             ?.filter(item => item != "")
+    //         audio = await sequelize.models.Audio.findAndCountAll({where: { keywords: {[Op.contains]: keywordsArr}}, limit, offset, order: [ [ 'createdAt', 'DESC' ]]})
+    //     } if(categoryId && keywords) {
+    //         const keywordsArr = keywords.trim().split(" ")
+    //             ?.map(item => item.trim().replace(",", "").toLowerCase())
+    //             ?.filter(item => item != "")
+    //         audio = await sequelize.models.Audio.findAndCountAll({where: { keywords: {[Op.contains]: keywordsArr}}, limit, offset, order: [ [ 'createdAt', 'DESC' ]]})
+    //     } if(!categoryId && !keywords) {
+    //         // audio = await sequelize.models.Audio.findAndCountAll({limit, offset})
+    //         audio = await sequelize.models.Audio.findAndCountAll({limit, offset, order: [ [ 'createdAt', 'DESC' ]]})
+    //     }
+    // } catch(e) {
+    //     console.error(`\n [Audio controller] Error \n`, e)
+    // }
         let {categoryId, keywords, limit, page} = req.query
         // const lookupValue = keywords?.toLowerCase()
 
@@ -68,9 +95,6 @@ const getAll = async (req, res) => {
             // audio = await sequelize.models.Audio.findAndCountAll({limit, offset})
             audio = await sequelize.models.Audio.findAndCountAll({limit, offset, order: [ [ 'createdAt', 'DESC' ]]})
         }
-    } catch(e) {
-        console.error(`\n [Audio controller] Error \n`, e)
-    }
     return res.json(audio)
 }
 
